127c6a634361826c15930e8b4aed535c6a4730e0
kryyyaaaa <137829849+kryyyaaaa@users.noreply.github.com>
2023-06-26 23:08:26 +0300

    Update cmt.py

--- cmt.py
+++ cmt.py
@@ -1,9 +1,12 @@
 import time
 import random
 
+print('')
+print('')
 print('cryptoMineTermux')
 k = input('ur bitckoin wallet: ')
 for i in range(10000000):
 	r = random.random()
 	time.sleep(0.9)
+	print('')
 	print('[CMT] btc{', k, '} usd:', r)
